9145e6c298797dc7dadaca72102779057b23c6d2
homi420 <rockyman2096@gmail.com>
2024-07-29 11:23:25 +0500

    path fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route element={<Home />} exact path="/home" />
+        <Route element={<Home />} exact path="/" />
       </Routes>
     </>
   );
